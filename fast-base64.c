6012561525649ed9924c3ebd1d14832803e1a130
Noel O'Donnell <6617714+odonnellnoel@users.noreply.github.com>
2019-09-09 01:12:41 +0100

    Padding and un-padding working, looks good overall

--- fast-base64.c
+++ fast-base64.c
@@ -1,4 +1,5 @@
 #include <string.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -6,89 +7,106 @@ typedef unsigned char BYTE;
 
 #include "lookup_tables.h"
 
-#define BUFSIZE 1024 * 1024
+/* 
+    Buffer size is measured in chunks. Each chunk is either
+    3 bytes of raw data, or 4 bytes of base64 characters.
+*/
+#define BUF_CHUNKS 100
 
-static BYTE buf[BUFSIZE];
-static char out[(BUFSIZE * 5) / 3];	// Technically 4/3 of input, but take some margin
+const static size_t DATA_BUF_SIZE = BUF_CHUNKS * 3;
+const static size_t B64_BUF_SIZE = BUF_CHUNKS * 4;
+
 
 void encode_base64(
     const BYTE* data,
     const size_t len,
     char* out,
-    size_t* out_len)
+    size_t* nout)
 {
-    size_t slen = (len / 3) * 4;
     BYTE* dp = (BYTE*) data - 1;
-    BYTE* dl = dp + len;
+    BYTE* dl = (BYTE*) dp + len;
     char* op = out - 1;
 
-    // for (size_t ic = 0; dp < dl;) {
-    //     out[ic++] = C0_ENCODE_TABLE[*(++dp)];
-    //     out[ic++] = C1_ENCODE_TABLE[*((unsigned short*)(dp))];
-    //     out[ic++] = C2_ENCODE_TABLE[*((unsigned short*)(++dp))];
-    //     out[ic++] = C3_ENCODE_TABLE[*(++dp)];
-    // }
-
-    for (size_t ic = 0; dp < dl;) {
-        *(++op) = C0_ENCODE_TABLE[*(++dp)];
-        *(++op) = C1_ENCODE_TABLE[*((unsigned short*)(dp))];
-        *(++op) = C2_ENCODE_TABLE[*((unsigned short*)(++dp))];
-        *(++op) = C3_ENCODE_TABLE[*(++dp)];
+    /* main encoding loop */
+    while (dp < dl) {
+        *(++op) = C0_ENCODE_TABLE[*++dp];
+        *(++op) = C1_ENCODE_TABLE[*((unsigned short*) dp )]; 
+        *(++op) = C2_ENCODE_TABLE[*((unsigned short*) ++dp )];
+        *(++op) = C3_ENCODE_TABLE[*++dp];
     }
 
-    *out_len = slen;
+    /* pad */
+    switch (dp - dl) {
+        case 1:
+            /* 2/3 byte block - re-compute b64 character c2 with mask and add 1 padding character */
+            *(op - 1) = C2_ENCODE_TABLE[*((unsigned short*)(dp - 1)) & 0x00FF];
+            *(op) = '=';
+            break;
+        case 2:
+            /* 1/3 byte block - re-compute b64 character c1 with mask and add 2 padding characters */
+            *(op - 2) =  C1_ENCODE_TABLE[*((unsigned short*)(dp - 2)) & 0x00FF];
+            *(op - 1) = '=';
+            *(op) = '=';
+            break;
+    }
+ 
+    *nout = (op - out) + 1;
 }
 
 void decode_base64(
     const char* b64,
-    BYTE** out,
-    size_t* out_len)
+    const size_t len,
+    BYTE* out,
+    size_t* nout)
 {
-    size_t slen = strlen(b64);
-    size_t len = (slen / 4) * 3;
-    BYTE* buf = (BYTE*) malloc(len);
+    char* sp = (char*) b64 - 1;
+    char* sl = (char*) sp + len;
+    BYTE* op = out - 1;
 
-    for (size_t ib = 0, ic = 0; ib < len; ib += 3, ic += 4) {
-        buf[ib + 0] = B0_DECODE_TABLE[*((unsigned short*) (b64 + ic + 0))];
-        buf[ib + 1] = B1_DECODE_TABLE[*((unsigned short*) (b64 + ic + 1))];
-        buf[ib + 2] = B2_DECODE_TABLE[*((unsigned short*) (b64 + ic + 2))];
+    /* main decoding loop */
+    while (sp < sl) {
+        *(++op) = B0_DECODE_TABLE[*((unsigned short*)(++sp))];
+        *(++op) = B1_DECODE_TABLE[*((unsigned short*)(++sp))];
+        *(++op) = B2_DECODE_TABLE[*((unsigned short*)(++sp))];
+        sp++;
     }
 
-    *out = buf;
-    *out_len = len;
+    /* up-pad - just reduce the # of bytes reported. No need to mess with the data. */
+    if (*sp == '=') {
+        if (*(sp - 1) == '=') {
+            *nout = (op - out) - 1;
+        } else {
+            *nout = (op - out);
+        }
+    } else {
+        *nout = (op - out) + 1;
+    }
 }
 
-int main(int argc, char** argv) 
+int main(
+    int argc, 
+    char** argv) 
 {
-    FILE* fp = stdin;
-    size_t nread;
-    size_t nout;
+    BYTE data_buf[DATA_BUF_SIZE];
+    char b64_buf[B64_BUF_SIZE];
+    size_t nread, nout;
+    int encode = 0;
 
-    while ((nread = fread(buf, 1, BUFSIZE, fp)) > 0) {
-        encode_base64(buf, nread, out, &nout);
-        fwrite(out, nout, 1, stdout);
+    if (encode) {
+        while ((nread = fread(data_buf, 1, DATA_BUF_SIZE, stdin)) > 0) {
+            encode_base64(data_buf, nread, b64_buf, &nout);
+            fwrite(b64_buf, nout, 1, stdout);
+        }
+    } else {
+        while ((nread = fread(b64_buf, 1, B64_BUF_SIZE, stdin)) > 0) {
+            decode_base64(b64_buf, nread, data_buf, &nout);
+
+            if (!write(1, data_buf, nout)) {
+                fprintf(stderr, "oh");
+            }
+        }
     }
-
-    // //const BYTE data[] = "The Magic Words are Squeamish Ossifrage";
-    // const size_t NDATA = 300000000;
-    // BYTE* data = (BYTE*) malloc(NDATA);
-    // char* b64 = (char*)0;
-    // BYTE* dec = (BYTE*)0;
-    // size_t dec_len;
-
-    // encode_base64(data, /*sizeof(data)*/ NDATA, &b64);
-    // puts(b64);
-
-    // //decode_base64(b64, &dec, &dec_len);
-
-    // //for (size_t i = 0; i < dec_len; i++) {
-    // //    printf("%c", dec[i]);
-    // //}
-    // //printf("\n");
-
-    // free(b64);
-    // //free(dec);
-    // free(data);
+    puts("");
 
     return 0;
 }
